d2c92a926962a12f7e4e2213583f38def03d0334
tsdg112 <hpkhpk@yahoo.com>
2023-10-07 17:14:30 -0400

    remove deprecated method

--- gvoice-ff.py
+++ gvoice-ff.py
@@ -9,8 +9,7 @@ from selenium.common.exceptions import NoSuchElementException
 
 options = Options()
 options.add_argument("-headless")
-driver = webdriver.Firefox(options=options)
-# driver = webdriver.Firefox(options=options, executable_path=r'C:\hpk\cd\geckodriver.exe')
+driver = webdriver.Firefox(options=options, executable_path=r'C:\hpk\cd\geckodriver.exe')
 driver.set_window_size(1024, 768)
 
 driver.implicitly_wait(1)
@@ -21,7 +20,7 @@ input_0 = False
 for _ in range(30):
 	time.sleep(0.5)
 	try:
-		driver.find_element_by_id('il1')
+		driver.find_element('id', 'il1')
 		break
 	except:
 		pass
@@ -29,11 +28,11 @@ for _ in range(30):
 	doc=driver.page_source.encode('ascii', 'replace').decode('ascii')
 	p = re.search('Sign in to continue to Google Voice|Sign in with your Google Account', doc)
 	if p:
-		driver.find_element_by_id('Email').send_keys(sys.argv[1])
-		driver.find_element_by_id('next').click()
+		driver.find_element('name', 'Email').send_keys(sys.argv[1])
+		driver.find_element('id', 'next').click()
 		time.sleep(1)
 
-		element = driver.find_element_by_xpath("//*[contains(text(), 'Enter your password')]")
+		element = driver.find_element('xpath', "//*[contains(text(), 'Enter your password')]")
 		actions = ActionChains(driver) 
 		actions.move_to_element_with_offset(element, 1, 1)
 		actions.send_keys(sys.argv[2])
@@ -47,7 +46,7 @@ for _ in range(30):
 	if p:
 		print("MAKE A CALL", p.group(1))
 		time.sleep(1)
-		element = driver.find_element_by_class_name(p.group(1))
+		element = driver.find_element("class_name", p.group(1))
 		driver.execute_script("arguments[0].click();", element)
 		break
 
@@ -60,7 +59,7 @@ for _ in range(30):
 
 driver.implicitly_wait(10)
 try:
-	driver.find_element_by_id('il1').send_keys(sys.argv[3])
+	driver.find_element('id', 'il1').send_keys(sys.argv[3])
 except:
 	print("GATE1")
 	print(driver.page_source.encode('ascii', 'replace').decode('ascii'))
@@ -69,21 +68,21 @@ except:
 	sys.exit()
 	
 driver.implicitly_wait(10)
-driver.find_element_by_xpath('//*[@gv-test-id="new-call-button"]').click()
-element = driver.find_element_by_xpath('//*[@role="listbox"]')
+driver.find_element('xpath','//*[@gv-test-id="new-call-button"]').click()
+element = driver.find_element('xpath', '//*[@role="listbox"]')
 driver.execute_script("arguments[0].click();", element)
-element = driver.find_element_by_xpath('//*[@role="option"]')
+element = driver.find_element('xpath', '//*[@role="option"]')
 driver.execute_script("arguments[0].click();", element)
 actions = ActionChains(driver) 
 actions.move_to_element_with_offset(element, 6, 6)
 actions.click()
 actions.perform()
-element = driver.find_element_by_xpath("//*[contains(text(), 'Connect')]")
+element = driver.find_element('xpath', "//*[contains(text(), 'Connect')]")
 driver.execute_script("arguments[0].click();", element)
 
 time.sleep(0.3)
 try:
-	element = driver.find_element_by_xpath('//button[normalize-space()="Connect"]')
+	element = driver.find_element('xpath', '//button[normalize-space()="Connect"]')
 	driver.execute_script("arguments[0].click();", element)
 except:
 	pass
